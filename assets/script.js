21ad3dfec81539dbb937ca8be77b681fe6512fe2
Moncrieffe2 <moncrieffea2@gmail.com>
2026-05-13 15:39:47 -0400

    Counter was entered with the wrong code

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,21 @@
 document.getElementById('year').textContent = new Date().getFullYear();
 
-<p class="counter-text">
-  This page has been viewed
-  <span class="counter-number">Loading...</span>
-  times
-</p>
+const counter = document.querySelector(".counter-number");
+
+async function updateCounter() {
+  try {
+    let response = await fetch(
+      "https://lsofygtvdyob3gnjki2vcl3axy0octlp.lambda-url.us-east-1.on.aws/"
+    );
+
+    let data = await response.json();
+
+    counter.innerHTML = data;
+  } catch (error) {
+    console.error("Counter failed:", error);
+
+    counter.innerHTML = "Unavailable";
+  }
+}
+
+updateCounter();
