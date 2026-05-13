62cc085fb763d5d555afe70ac8e3b5090a5c8e7d
Moncrieffe2 <moncrieffea2@gmail.com>
2026-05-13 15:27:20 -0400

    This is the new code for the visitor counter. gonna change the color later to make it stand out but this will be okay for now.

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,7 @@
 document.getElementById('year').textContent = new Date().getFullYear();
+
+<p class="counter-text">
+  This page has been viewed
+  <span class="counter-number">Loading...</span>
+  times
+</p>
